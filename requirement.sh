904e55bf5b49eb13ef865ca6990c9a8a1a04eeec
Keval Malde <maldekeval@ymail.com>
2022-02-27 23:36:49 +0530

    Create requirement.sh
    Shell script for installing depedency

---
+++ requirement.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+npm install express
+npm install express-session
+npm install passport
+npm install passport-google-oauth2
+npm install socket.io
+npm install cookie-session
+npm install ejs
+npm install pg
+npm install googleapis@39 --save
